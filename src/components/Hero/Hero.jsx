764be133130a58c57b7e11b8d2c2316accef6f15
tonmoy5 <tonmoy52532@gmail.com>
2023-02-23 22:06:59 +0600

    Minor adjustments

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,9 @@ import Particle from './Particle'
 
 const Hero = () => {
     return (
-        <div className='w-full flex md:justify-center justify-between items-center p-4 h-[70vh] md:h-[calc(100vh-100px)] lg:h-[calc(100vh-100px)] hero'>
-
+        <div className='w-full flex md:justify-end justify-between items-center h-[70vh] md:h-[calc(100vh-100px)] lg:h-[calc(100vh-100px)] hero '>
             <Particle />
-            <div className="container">
+            <div className="container bg-primary-400">
                 <div className="row">
                     hello
                 </div>
