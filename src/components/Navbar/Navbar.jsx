826c6526894479691734cda80d8d7fab8cd8973b
Md Tonmoy <tanzinulkabir@gmail.com>
2023-03-01 10:14:01 +0600

    Minor Changes to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,6 @@ import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import logo from "../../assets/img/logo.png"
 
-const NavBarItem = ({ title, classprops, setToggleMenu }) => (
-    <Link to={`/${title}`}>
-        <li className={`mx-4 cursor-pointer uppercase text-white md:text-gray-400 text-xl hover:text-primary-400 ${classprops}`} onClick={() => setToggleMenu(false)}>{title}</li>
-    </Link>
-);
 
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = React.useState(false);
@@ -23,9 +18,21 @@ const Navbar = () => {
                 <p className="text-gray-500 hover:text-primary-400 hidden md:hidden lg:block italic">We make your Business Successfull</p>
             </div>
             <ul className="md:flex hidden list-none flex-row justify-between items-center flex-initial">
-                {["home", "about us", "Services", "Contact"].map((item, index) => (
+                {/* {["home", "about us", "Services", "Contact"].map((item, index) => (
                     <NavBarItem key={item + index} title={item} />
-                ))}
+                ))} */}
+                <Link to="/">
+                    <li className="mx-4 cursor-pointer uppercase text-white md:text-gray-400 text-xl hover:text-primary-400" onClick={() => setToggleMenu(false)}>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li className="mx-4 cursor-pointer uppercase text-white md:text-gray-400 text-xl hover:text-primary-400" onClick={() => setToggleMenu(false)}>About Us</li>
+                </Link>
+                <Link to="/services">
+                    <li className="mx-4 cursor-pointer uppercase text-white md:text-gray-400 text-xl hover:text-primary-400" onClick={() => setToggleMenu(false)}>Services</li>
+                </Link>
+                <Link to="/contact">
+                    <li className="mx-4 cursor-pointer uppercase text-white md:text-gray-400 text-xl hover:text-primary-400" onClick={() => setToggleMenu(false)}>Contact</li>
+                </Link>
                 <Link to='/apply' target="_blank">
                     <li className="bg-primary-500 py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-primary-400 text-white">
                         Aplly For Job
@@ -36,18 +43,30 @@ const Navbar = () => {
                 {!toggleMenu && (
                     <HiMenuAlt4 fontSize={28} className="text-gray-500 hover:text-primary-400 md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
                 )}
-                {/* {toggleMenu && (
-            <AiOutlineClose fontSize={28} className="text-gray-500 hover:text-primary-400 md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
-            )} */}
+                
                 {toggleMenu && (
                     <ul
                         className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
                 flex flex-col justify-start items-center rounded-md glassmorphism text-gray-500 hover:text-primary-400 animate-slide-in"
                     >
                         <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-                        {["home", "about us", "Services", "Contact"].map(
-                            (item, index) => <NavBarItem key={item + index} title={item} setToggleMenu={setToggleMenu} classprops="my-2 text-lg" />,
-                        )}
+                        <Link to="/">
+                            <li className="mx-4 cursor-pointer uppercase text-gray md:text-gray-400 text-xl hover:text-primary-400 mt-3" onClick={() => setToggleMenu(false)}>Home</li>
+                        </Link>
+                        <Link to="/about">
+                            <li className="mx-4 cursor-pointer uppercase text-gray md:text-gray-400 text-xl hover:text-primary-400 mt-3" onClick={() => setToggleMenu(false)}>About Us</li>
+                        </Link>
+                        <Link to="/services">
+                            <li className="mx-4 cursor-pointer uppercase text-gray md:text-gray-400 text-xl hover:text-primary-400 mt-3" onClick={() => setToggleMenu(false)}>Services</li>
+                        </Link>
+                        <Link to="/contact">
+                            <li className="mx-4 cursor-pointer uppercase text-gray md:text-gray-400 text-xl hover:text-primary-400 mt-3" onClick={() => setToggleMenu(false)}>Contact</li>
+                        </Link>
+                        <Link to='/apply' target="_blank">
+                            <li className="bg-primary-500 py-2 px-7 mx-4 mt-5 rounded-full cursor-pointer hover:bg-primary-400 text-white">
+                                Aplly For Job
+                            </li>
+                        </Link>
                     </ul>
                 )}
             </div>
