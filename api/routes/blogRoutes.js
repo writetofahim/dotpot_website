ff0877ecef74a23e4f8948c4ef6df31ac5abf6ad
Md Tonmoy <tanzinulkabir@gmail.com>
2023-03-13 11:38:02 +0600

    blog api ready

--- api/routes/blogRoutes.js
+++ api/routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const blogController = require('../controllers/blogController');
+const blogController = require("../controllers/blogController")
 
 router.get('', blogController.getAllBlogs);
 router.get('/:id', blogController.getBlogById);
