2a1c0eb721a29dfcdc65b9a8298efe2a65968f76
Fahim <writetofahimahammad@gmail.com>
2023-03-23 16:04:31 +0600

    changed design and navigation on sidebar

--- admin/src/partials/Sidebar.jsx
+++ admin/src/partials/Sidebar.jsx
@@ -223,14 +223,14 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
               {/* Orders */}
               <li
                 className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
-                  pathname.includes("orders") && "bg-slate-900"
+                  pathname.includes("order") && "bg-slate-900"
                 }`}
               >
                 <NavLink
                   end
                   to="/order"
                   className={`block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("orders")
+                    pathname.includes("job-applications")
                       ? "hover:text-slate-200"
                       : "hover:text-white"
                   }`}
@@ -247,87 +247,30 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
               </li>
 
               {/* Client Response */}
-              <SidebarLinkGroup
-                activecondition={pathname.includes("client-responses")}
+
+              <li
+                className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${
+                  pathname.includes("client-response/all-responses") &&
+                  "bg-slate-900"
+                }`}
               >
-                {(handleClick, open) => {
-                  return (
-                    <React.Fragment>
-                      <a
-                        href="#0"
-                        className={`block text-slate-200 truncate transition duration-150 ${
-                          pathname.includes("users")
-                            ? "hover:text-slate-200"
-                            : "hover:text-white"
-                        }`}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          sidebarExpanded
-                            ? handleClick()
-                            : setSidebarExpanded(true);
-                        }}
-                      >
-                        <div className="flex items-center justify-between">
-                          <div className="flex items-center">
-                            {/* <FiUsers /> */}
-                            <MdOutlineReviews />
-                            <span className="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                              Client Responses
-                            </span>
-                          </div>
-                          {/* Icon */}
-                          <div className="flex shrink-0 ml-2">
-                            <svg
-                              className={`w-3 h-3 shrink-0 ml-1 fill-current text-slate-400 ${
-                                open && "rotate-180"
-                              }`}
-                              viewBox="0 0 12 12"
-                            >
-                              <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
-                            </svg>
-                          </div>
-                        </div>
-                      </a>
-                      <div className="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                        <ul className={`pl-9 mt-1 ${!open && "hidden"}`}>
-                          <li className="mb-1 last:mb-0">
-                            <NavLink
-                              end
-                              to="/client-response/all-responses"
-                              className={({ isActive }) =>
-                                "block transition duration-150 truncate " +
-                                (isActive
-                                  ? "text-indigo-500"
-                                  : "text-slate-400 hover:text-slate-200")
-                              }
-                            >
-                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                All Response
-                              </span>
-                            </NavLink>
-                          </li>
-                          {/* <li className="mb-1 last:mb-0">
-                            <NavLink
-                              end
-                              to="/client-response/add-responses"
-                              className={({ isActive }) =>
-                                "block transition duration-150 truncate " +
-                                (isActive
-                                  ? "text-indigo-500"
-                                  : "text-slate-400 hover:text-slate-200")
-                              }
-                            >
-                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Add Response
-                              </span>
-                            </NavLink>
-                          </li> */}
-                        </ul>
-                      </div>
-                    </React.Fragment>
-                  );
-                }}
-              </SidebarLinkGroup>
+                <NavLink
+                  end
+                  to="/client-response/all-responses"
+                  className={`block text-slate-200 truncate transition duration-150 ${
+                    pathname.includes("job-applications")
+                      ? "hover:text-slate-200"
+                      : "hover:text-white"
+                  }`}
+                >
+                  <div className="flex items-center">
+                    <MdOutlineReviews />
+                    <span className="ml-3 text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      Client Response
+                    </span>
+                  </div>
+                </NavLink>
+              </li>
               {/* Users */}
               <SidebarLinkGroup activecondition={pathname.includes("users")}>
                 {(handleClick, open) => {
