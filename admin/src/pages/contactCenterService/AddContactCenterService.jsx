17018e525b5426a2def6e952b23d378c34e31729
Fahim <writetofahimahammad@gmail.com>
2023-03-23 13:44:00 +0600

    contact-center-service is done

--- admin/src/pages/contactCenterService/AddContactCenterService.jsx
+++ admin/src/pages/contactCenterService/AddContactCenterService.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
+import { useSearchParams } from "react-router-dom";
+import CommonSnackbar from "../../components/ComonSnackbar";
+import axios from "../../utils/axiosInstance";
 
 const AddContactCenterService = () => {
-  return (
-    <div>AddContactCenterService</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [icon, setIcon] = useState("");
+  const [message, setMessage] = useState("");
+  const [snackbar, setSnackbar] = useState(false);
+  const [searchParams] = useSearchParams();
+  const industryWeServeId = searchParams.get("id");
 
-export default AddContactCenterService
+  // fetching data
+  useEffect(() => {
+    if (industryWeServeId) {
+      axios
+        .get(`/contact_center_service/${industryWeServeId}`)
+        .then((res) => {
+          setTitle(res.data.title);
+          setIcon(res.data.icon);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [industryWeServeId]);
+
+  // handle Form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      if (industryWeServeId) {
+        await axios.put(`/contact_center_service/${industryWeServeId}`, {
+          title,
+          icon,
+        });
+        setMessage("Update Successful!");
+        setSnackbar(true);
+      } else {
+        await axios.post("/contact_center_service", {
+          title,
+          icon,
+        });
+        setMessage("Add Successful!");
+        setSnackbar(true);
+        setTitle("");
+        setIcon("");
+      }
+    } catch (err) {
+      alert("Error creating industry.");
+    }
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="max-w-lg mx-auto">
+      {snackbar && (
+        <CommonSnackbar
+          message={message}
+          open={snackbar}
+          setOpen={setSnackbar}
+        />
+      )}
+
+      <h1 className="text-center my-3">
+        {industryWeServeId ? "Update" : "Add"} industry we serve!
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Enter title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="icon">
+            Icon
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Enter icon"
+            value={icon}
+            onChange={(e) => setIcon(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700
+        text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center gap-2"
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading
+              ? industryWeServeId
+                ? "Updating..."
+                : "Adding..."
+              : industryWeServeId
+              ? "Update"
+              : "Add"}
+            {isLoading && <FaSpinner className="animate-spin" />}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddContactCenterService;
--- admin/src/pages/contactCenterService/AllContactCenterService.jsx
+++ admin/src/pages/contactCenterService/AllContactCenterService.jsx
@@ -1,44 +1,98 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import * as React from "react";
+import { AiOutlineEdit } from "react-icons/ai";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { Link, useNavigate } from "react-router-dom";
+import DeleteModal from "../../components/DeleteModal";
+import axios from "../../utils/axiosInstance";
 
 const AllContactCenterService = () => {
+  const [data, setData] = React.useState([]);
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = React.useState();
+  const [selectedId, setSelectedId] = React.useState();
+
+  const fetchData = async () => {
+    try {
+      // const response = await axios.get(`/client_review?page=${page}`);
+      const response = await axios.get("/contact_center_service");
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+  const handleDelete = async (id) => {
+    const response = await axios.delete(`/contact_center_service/${id}`);
+    if (response.status === 200) {
+      setData((existing) => existing.filter((d) => d._id !== id));
+    }
+  };
+  const deleteService = (id) => {
+    setSelectedId(id);
+    setIsModalOpen(true);
+  };
   return (
     <div className="relative overflow-x-auto">
-      <Link to="/clients-review/add-clients-review">
-        <button className="m-2 px-3 py-2 bg-green-400 text-white font-bold cursor-pointer rounded-lg hover:scale-105 hover:shadow-xl transition-all">
+      {isModalOpen && (
+        <DeleteModal
+          handleDelete={() => handleDelete(selectedId)}
+          open={isModalOpen}
+          setOpen={setIsModalOpen}
+        />
+      )}
+      <Link to="/contact-center-service/add-contact-center-service">
+        <button className="m-5 px-3 py-2 bg-green-400 text-white font-bold cursor-pointer rounded-lg hover:scale-105 hover:shadow-xl transition-all">
           Add New
         </button>
       </Link>
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <table className="mb-14 w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
-              Image
+              Icon
             </th>
             <th scope="col" className="px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Company Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Position
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Review
+              Title
             </th>
             <th scope="col" className="px-6 py-3">
               Action
             </th>
           </tr>
         </thead>
-        {/* <tbody>
-            {data?.map((clientsReview) => (
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={clientsReview._id}>
-                <ClientsReviewTableRow clientsReview={clientsReview} setData={setData} data={data} />
-              </tr>
-            ))}
-          </tbody> */}
+        <tbody>
+          {data?.map((service) => (
+            <tr
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              key={service._id}
+            >
+              <td className="px-6 py-4">
+                <img className="w-24" src={service.icon} alt="" />
+              </td>
+              <td className="px-6 py-4">{service.title}</td>
+              <td className="px-6 py-2 ">
+                <div className="flex gap-3">
+                  <AiOutlineEdit
+                    onClick={() => {
+                      navigate(
+                        `/contact-center-service/add-contact-center-service?id=${service._id}`
+                      );
+                      console.log("clicked");
+                    }}
+                    className="text-violet-500 cursor-pointer"
+                  />
+                  <RiDeleteBinLine
+                    onClick={() => deleteService(service._id)}
+                    className="text-red-500 cursor-pointer"
+                  />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
