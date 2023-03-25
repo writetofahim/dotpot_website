844dfb9fa6a94f2fefc7164220411de96d944128
Md Tonmoy <tanzinulkabir@gmail.com>
2023-03-25 12:53:50 +0600

    PrivacyPolicy & TermsAndConditions PDF added

--- client/src/pages/privacyPolicy/PrivacyPolicy.jsx
+++ client/src/pages/privacyPolicy/PrivacyPolicy.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import privacy from '../../assets/pdfs/privacy.pdf'
+import Footer from '../../components/Footer/Footer'
 
 const PrivacyPolicy = () => {
   return (
-    <div>PrivacyPolicy</div>
+    <div className="w-full">
+      <Navbar />
+
+      {/* Main Div */}
+      <div className="pt-24 w-full min-h-[100vh] flex items-center justify-center">
+          
+        <iframe
+          src={privacy}
+          title="Terms and Conditions"
+          className="w-full h-screen"
+        />
+
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
--- client/src/terms_conditions/Terms_Conditions.jsx
+++ client/src/terms_conditions/Terms_Conditions.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Navbar from '../components/Navbar/Navbar'
+import terms from "../assets/pdfs/terms.pdf"
 
 const Terms_Conditions = () => {
   return (
-    <div>Terms_Conditions</div>
+    <div className="w-full">
+      <Navbar />
+
+      {/* Main Div */}
+      <div className="pt-24 w-full min-h-[100vh] flex items-center justify-center">
+          
+        <iframe
+          src={terms}
+          title="Terms and Conditions"
+          className="w-full h-screen"
+        />
+
+      </div>
+
+      <Footer />
+    </div>
   )
 }
 
