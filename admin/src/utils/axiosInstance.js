826c69500b1f98eefd79d443a2b7d139045c2509
Md Tonmoy <tanzinulkabir@gmail.com>
2023-03-27 14:07:04 +0600

    Api path changed to "http://dotpotit.com"

--- admin/src/utils/axiosInstance.js
+++ admin/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const token = localStorage.getItem("accessToken");
 const instance = axios.create({
-  baseURL: 'http://localhost:8800/api',
+  baseURL: 'http://dotpotit.com/api',
   headers:{'Authorization': `Bearer ${JSON.parse(token)}`},
 });
 
