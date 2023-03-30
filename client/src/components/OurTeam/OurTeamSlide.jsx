96b4ac464dd5c87b2c98ccef83386bb27cc2a671
Md Tonmoy <tanzinulkabir@gmail.com>
2023-03-30 15:27:09 +0600

    environment variable added in Admin

--- client/src/components/OurTeam/OurTeamSlide.jsx
+++ client/src/components/OurTeam/OurTeamSlide.jsx
@@ -88,7 +88,7 @@ var settings = {
 export const SingleSlide = (props) => {
     return (
         <div className="mx-5 mb-20 rounded-lg p-5 bg-white flex items-center flex-col shadow-xl cursor-pointer hover:border hover:border-primary-400 hover:scale-x-105 transition-all">
-            <img src={`${import.meta.env.REACT_APP_SERVER_PATH}/${props.image}`} alt="" className=" w-2/5 aspect-square rounded-full" />
+            <img src={`${import.meta.env.REACT_APP_SERVER_PATH}/${props.image}`} alt="" className=" w-2/5 aspect-square object-contain rounded-full" />
             <a href={props.link}>
                 <h3 className="mt-2 text-xl font-bold text-gray-400">{props.name}</h3>
             </a>
