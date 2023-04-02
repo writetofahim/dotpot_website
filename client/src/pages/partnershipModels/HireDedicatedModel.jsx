237fa38afe4b714b29c2c39d7bdf0abd5532e4a7
SHSujat <haque.shamshul27@gmail.com>
2023-04-02 16:09:08 +0600

    Updated Partnership Model Pages

--- client/src/pages/partnershipModels/HireDedicatedModel.jsx
+++ client/src/pages/partnershipModels/HireDedicatedModel.jsx
@@ -13,33 +13,33 @@ const HireDedicatedModel = () => {
           </div>
           <div className="my-12 lg:mx-24 mx-6">
             <h2 className="lg:text-2xl text-xl font-bold mb-4">Hire Dedicated Partnership Model</h2>
-            <p className="text-xl">
+            <p className="text-lg">
               Dotpot iT is a rapidly growing technology company that offers a wide range of IT services, including software development, mobile app development, web development, and digital marketing. To keep up with the demand for its services, the company needs a team of skilled professionals who can work together to deliver high-quality solutions to its clients. This is where hiring a dedicated model comes in.
             </p>
-            <p className="text-xl py-2">
+            <p className="text-lg py-2">
               A dedicated model is a hiring strategy where a company hires a team of professionals to work exclusively on its projects. The team is dedicated to the company and works under its management, but the company does not have to provide the team with physical office space, hardware, or other resources. The team can work remotely, and the company can monitor their progress through various communication channels.
             </p>
             <div className="my-6">
               <h2 className="lg:text-2xl text-xl font-semibold mb-4">Benefits of hiring a dedicated partnership model for Dotpot iT:</h2>
               <ul className="list-disc list-inside">
-              <li className="mb-2">
+              <li className="mb-2 text-lg">
                   <strong className="font-semibold">Access to skilled professionals:</strong> When you hire a dedicated model, you get access to a team of skilled professionals who have experience working in your industry. This means that they can quickly understand your requirements and deliver solutions that meet your needs.
                 </li>
-                <li className="mb-2">
+                <li className="mb-2 text-lg">
                   <strong className="font-semibold">Cost-effective:</strong> Hiring a dedicated model can be more cost-effective than hiring a team of full-time employees. This is because you do not have to pay for benefits, office space, or hardware. Instead, you pay a fixed monthly fee for the team's services.
                 </li>
-                <li className="mb-2">
+                <li className="mb-2 text-lg">
                   <strong className="font-semibold">Scalability:</strong> With a dedicated model, you can easily scale up or down your team size based on your project requirements. This means that you can quickly respond to changes in demand without having to worry about hiring or firing employees.
                 </li>
-                <li className="mb-2">
+                <li className="mb-2 text-lg">
                   <strong className="font-semibold">Flexibility:</strong> A dedicated model gives you the flexibility to work with a team of professionals who are located in different time zones. This means that you can extend your working hours and deliver projects faster.
                 </li>
-                <li>
+                <li className="mb-2 text-lg">
                   <strong className="font-semibold">Control:</strong> When you hire a dedicated model, you have complete control over the team's work. You can monitor their progress, provide feedback, and make changes as needed.
                 </li>
               </ul>
             </div>
-            <p className="text-xl">
+            <p className="text-lg">
               In conclusion, hiring a dedicated model is a great option for Dotpot iT. It can help the company access skilled professionals, reduce costs, and increase flexibility and control over its projects. With a dedicated model, Dotpot iT can continue to deliver high-quality solutions to its clients and maintain its competitive edge in the market.
             </p>
           </div>
