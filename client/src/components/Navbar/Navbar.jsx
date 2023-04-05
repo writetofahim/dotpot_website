fec6609315ed97673afc157ddfb11ab9cbdab4a5
motalibpathan <motalibpathan11@gmail.com>
2023-04-05 12:20:41 +0600

    Navbar dropdown added

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -11,9 +11,9 @@ To customize the links or add new ones, simply modify the Link components and th
 To apply custom styles, modify the with tailwind css classes and the background style is in index.css file.
  */
 
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
-import { IoIosArrowDown } from "react-icons/io"
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io"
 import { AiOutlineClose } from "react-icons/ai";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/img/logo.png"
@@ -24,42 +24,110 @@ const Navbar = () => {
     const [toggleMenu, setToggleMenu] = useState(false);
     const [isDropDownOpen, setIsDropDownOpen] = useState(false);
     const { user, login, error, logout } = useContext(AuthContext);
+    const [isSubmenuOpen, setIsSubmenuOpen]=useState(false)
+    const [selectedSubmenu, setSelectedSubmenu]=useState(null)
 
     const services = [
         {
-            _id: "64154c0fa6d33ade2d34b7c4",
-            to: "/",
+            _id: 1,
             title: "App Development",
             icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",
+            submenu:[
+                {to: "/", title: "Android App", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "iPhone App", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Flutter App", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Kotlin", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Iconic", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Swift", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+            ]
         },
         {
-            _id: "64154c31a6d33ade2d34b7cb",
-            to: "/",
+            _id:2,
             title: "Web Development",
             icon: "https://cdn-icons-png.flaticon.com/512/1927/1927656.png",
+            submenu:[
+                {to: "/", title: "React JS", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Next JS", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "PHP", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Wordpress", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Drupal", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+                {to: "/", title: "Django", icon: "https://cdn-icons-png.flaticon.com/512/545/545245.png",},
+            ]
 
         },
         {
-            _id: "64154c54a6d33ade2d34b7f0",
-            to: "/",
+            _id:3,
+            title: "Blockchain Development",
+            icon: "https://cdn-icons-png.flaticon.com/512/2152/2152349.png",
+            submenu:[
+                {to: "/", title: "Wallet"},
+                {to: "/", title: "Exchange"},
+                {to: "/", title: "Ethereum"},
+                {to: "/", title: "Smart Contacts"},
+                {to: "/", title: "Private Blockchain"},
+                {to: "/", title: "NFT Marketplace"},
+            ]
+
+        },
+        {
+            _id: 4,
+            title: "AI & ML",
+            icon: "https://cdn-icons-png.flaticon.com/512/2152/2152349.png",
+            submenu: [
+                { to: "/", title: "Object Recognition" },
+                { to: "/", title: "Text to speech" },
+                { to: "/", title: "Business Intelligence" },
+                { to: "/", title: "Data Forecasting" },
+                { to: "/", title: "Natural Language Processing" },
+                { to: "/", title: "Data Analytics" },
+                { to: "/", title: "Recommendation Engine" },
+            ]
+        },
+        {
+            _id: 5,
             title: "iT Support",
             icon: "https://cdn-icons-png.flaticon.com/512/2058/2058768.png",
+            submenu: [
+                { to: "/", title: "Cloud Computing" },
+                { to: "/", title: "Database Administration" },
+                { to: "/", title: "Cybersecurity " },
+                { to: "/", title: "IT Consulting" },
+                { to: "/", title: "Project Management" },
+                { to: "/", title: "Technical Support" },
 
+            ]
         },
         {
-            _id: "64154c6fa6d33ade2d34b81f",
-            to: "/",
+            _id: 6,
             title: "Contact Center",
             icon: "https://cdn-icons-png.flaticon.com/512/1687/1687136.png",
+            submenu: [
+                { to: "/contact_center_services/cti", title: "CTI" },
+                { to: "/contact_center_services/email-marketing", title: "Email Marketing" },
+                { to: "/contact_center_services/chat", title: "Chat" },
+                { to: "/contact_center_services/pd", title: "Predictive Dialer" },
+                { to: "/contact_center_services/mobile-app", title: "Mobile App Module" },
+                { to: "/contact_center_services/collection", title: "Collection" },
+                { to: "/contact_center_services/customer-service", title: "Customer Service" },
+                { to: "/contact_center_services/ivr", title: "IVR" },
+                { to: "/contact_center_services/acd", title: "ACD" },
+            ]
 
         },
         {
-            _id: "641ad843606377b65fcb8639",
-            to: "/",
+            _id: 7,
             title: "Game Development",
             icon: "https://cdn-icons-png.flaticon.com/512/6581/6581049.png",
+            submenu:[
+                {to: "/", title: "Unity 3D", },
+                {to: "/", title: "Metaverse"},
+                {to: "/", title: "Unreal Engine"},
+                {to: "/", title: "Augmented Reality"},
+                {to: "/", title: "Casual Games"},
+            ]
         }
     ]
+    
 
     return (
         <div className="w-full flex items-center justify-center fixed z-50 bg-white">
@@ -70,13 +138,13 @@ const Navbar = () => {
                     </Link>
                     <p className="text-gray-500 hover:text-primary-400 hidden lg:hidden lg:block italic">We make your Business Successful</p>
                 </div>
-                <ul className="lg:flex hidden list-none flex-row justify-between items-center flex-initial">
+                <div className="lg:flex hidden list-none flex-row justify-between items-center flex-initial">
                     {/* {["home", "about us", "Services", "Contact"].map((item, index) => (
                         <NavBarItem key={item + index} title={item} />
                     ))} */}
                     <NavLink to="/">
                         {({ isActive }) => (
-                            <li className={`${isActive ? "lg:text-primary-400 text-white" : "lg:text-gray-400 "} mx-4 cursor-pointer uppercase font-bold  text-lg hover:scale-110 hover:text-primary-400`}>Home</li>
+                            <span className={`${isActive ? "lg:text-primary-400 text-white" : "lg:text-gray-400 "} mx-4 cursor-pointer uppercase font-bold  text-lg hover:scale-110 hover:text-primary-400`}>Home</span>
                         )}
                     </NavLink>
                     <NavLink to="/about">
@@ -84,17 +152,19 @@ const Navbar = () => {
                             <li className={`${isActive ? "lg:text-primary-400 text-white" : "lg:text-gray-400 "} mx-4 cursor-pointer uppercase font-bold  text-lg hover:scale-110 hover:text-primary-400`}>About Us</li>
                         )}
                     </NavLink>
-                    <NavLink to="/services" className={"relative group "}>
-                        {({ isActive }) => (
-                            <>
-                                <li className={`${isActive ? "lg:text-primary-400 text-white" : "lg:text-gray-400 "} mx-4 cursor-pointer uppercase font-bold  text-lg hover:scale-110 hover:text-primary-400 flex gap-2 items-center`}>Services <IoIosArrowDown /></li>
-                                <span className="absolute hidden group-hover:flex top-6 left-0  flex-col gap-3 py-5 bg-white px-5 rounded-md shadow-2xl">
-                                    {services.map((service, i) => <Link key={i} to={service.to} className="flex gap-2 items-center w-max hover:scale-105 duration-100"><img width={20} src={service.icon} alt="" /><span key={service._id} className="w-full hover:text-primary-400 font-[600]">{service.title}</span></Link>)}
-                                </span>
-                            </>
-                        )}
-
-                    </NavLink>
+                    <div className={"relative w-max"}>
+                        <li className={` mx-4 cursor-pointer uppercase font-bold text-lg hover:scale-110 hover:text-primary-400 flex gap-2 items-center`} onClick={() => setIsSubmenuOpen(p => !p)}>Services <IoIosArrowDown /></li>
+                        <div className={`absolute ${isSubmenuOpen ? "flex" : "hidden"} top-6 left-0 w-max flex-col gap-3 py-5 bg-white rounded-md shadow-2xl`}>
+                            {services.map((service, i) => <div key={i} className="px-5 flex group/item gap-2 items-center w-full hover:scale-105 duration-100 cursor-pointer relative">
+                                <img className="" width={20} src={service.icon} alt="" />
+                                <div key={service._id} className="w-full hover:text-primary-400 font-[600] flex items-center gap-2">{service.title} <IoIosArrowForward />
+                                </div>
+                                <div className="group-hover/item:flex duration-500 h-0 transition-all group-hover/item:h-max flex-col gap-2 hidden absolute -top-2 lg:left-[275px] bg-white p-3 rounded-md shadow-xl">
+                                    {service?.submenu?.map((item, i) => <Link key={i} to={item.to} className="w-[200px] hover:text-primary-400" >{item.title}</Link>)}
+                                </div>
+                            </div>)}
+                        </div>
+                    </div>
                     <NavLink to="/contact">
                         {({ isActive }) => (
                             <li className={`${isActive ? "lg:text-primary-400 text-white" : "lg:text-gray-400 "} mx-4 cursor-pointer uppercase font-bold  text-lg hover:scale-110 hover:text-primary-400`} onClick={() => setToggleMenu(false)}>Contact</li>
@@ -122,7 +192,7 @@ const Navbar = () => {
                             Apply For Jobs
                         </li>
                     </Link>
-                </ul>
+                </div>
                 <div className="flex relative lg:hidden">
                     {!toggleMenu && (
                         <HiMenuAlt4 fontSize={28} className="text-gray-500  lg:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
@@ -143,10 +213,18 @@ const Navbar = () => {
                             <Link to="/services">
                                 <li className="mx-4 cursor-pointer uppercase text-gray lg:text-gray-400 text-xl font-bold mt-3 flex gap-2 items-center" onClick={() => setIsDropDownOpen(p => !p)}>Services <IoIosArrowDown /></li>
                             </Link>
-                            <div className={`relative overflow-hidden ${!isDropDownOpen ? "h-0" : "h-[250px]"} duration-500 transition-all text-center`}>
-                                {services.map(service => <Link key={service._id} to={service.to}>
-                                    <li className="mx-4 cursor-pointer uppercase text-gray lg:text-gray-400 text-lg font-bold mt-3" onClick={() => setToggleMenu(false)}>{service.title} </li>
-                                </Link>)}
+                            <div className={`relative overflow-hidden ${!isDropDownOpen ? "h-0" : "h-max"} duration-500 transition-all text-center`}>
+                                {services.map((service, i) => <div key={i} >
+                                    <div key={i} className={`mx-4 cursor-pointer uppercase text-gray lg:text-gray-400 text-base font-bold mt-3 relative` }>
+                                        <span onClick={() => setSelectedSubmenu(service._id)} className={`${selectedSubmenu === service._id ? "text-blue-700" : ""} flex gap-2 items-center justify-center`}>{service.title} <IoIosArrowDown /></span>
+                                        <div className={`${selectedSubmenu === service._id ? `h-max` : "max-h-0"} flex flex-col gap-2 overflow-hidden transition-max-h duration-500`}>
+                                            <div className="h-full" >
+                                                {service?.submenu?.map((item, index) => <Link key={i+index} to={item.to} className={`w-max mx-auto hover:text-primary-400 text-sm`} >{item.title}</Link>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>)}
                                 <Link to="/services">
                                     <li className="mx-4 cursor-pointer uppercase text-gray lg:text-gray-400 text-lg font-bold mt-3" onClick={() => setToggleMenu(false)}>Get A Quote </li>
                                 </Link>
@@ -178,3 +256,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
