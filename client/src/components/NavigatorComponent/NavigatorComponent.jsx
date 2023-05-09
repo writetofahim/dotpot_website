84cdb96c79cca966686f85f3bc5c00657360b6c0
Md Tonmoy <tanzinulkabir@gmail.com>
2023-05-09 11:29:40 +0600

    Navigator position changed

--- client/src/components/NavigatorComponent/NavigatorComponent.jsx
+++ client/src/components/NavigatorComponent/NavigatorComponent.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const NavigatorComponent = ({ navigationData }) => {
   console.log(navigationData);
   return (
-    <div className="container pt-2 mx-auto pb-5 md:px-5 px-1 flex items-center">
+    <div className="container pt-5 md:pt-2 lg:pt-1 mx-auto pb-5 md:px-5 px-1 flex items-center">
       <div className="w-max flex text-xl backdrop-blur-lg">
         <Link
           to="/"
-          className="ml-2 text-textColor-500 hover:text-textColor-500 md:text-base text-xs"
+          className="ml-2 text-textColor-500 hover:text-secondary-500 md:text-base text-xs"
         >
           Home
         </Link>
@@ -21,7 +21,7 @@ const NavigatorComponent = ({ navigationData }) => {
             <Link
               to={item.link}
               key={index}
-              className="ml-2 hover:text-textColor-500 "
+              className="ml-2 hover:text-secondary-500"
             >
               {item.title}
             </Link>{" "}
