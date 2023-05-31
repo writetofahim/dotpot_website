68056adba73b50eb920ddac642c3e7f559735bc1
Md Tonmoy <tanzinulkabir@gmail.com>
2023-05-31 16:28:45 +0600

    c_heroController and ourPartnerController warning resolved

--- api/controllers/c_heroController.js
+++ api/controllers/c_heroController.js
@@ -1,72 +1,93 @@
-const Hero = require('../models/C_hero');
-const { removeFile } = require('../utilities/removeFile');
+// Import the Hero model
+const Hero = require("../models/C_hero");
 
-// Create a new hero
+// Create a new hero and save it to the database
 exports.createHero = async (req, res) => {
   try {
+    // Create a new hero instance from the request body
     const hero = new Hero(req.body);
+    // Save the hero to the database and return it in the response
     const savedHero = await hero.save();
     res.status(201).json(savedHero);
   } catch (error) {
+    // Send a 400 response with the error message if there is an error
     res.status(400).json({ message: error.message });
   }
 };
 
-// Get all heroes
+// Get all heroes from the database
 exports.getAllHeroes = async (req, res) => {
   try {
+    // Find all heroes in the database and return them in the response
     const heroes = await Hero.find();
     res.json(heroes);
   } catch (error) {
+    // Send a 500 response with the error message if there is an error
     res.status(500).json({ message: error.message });
   }
 };
 
-// Get a single hero by id
+// Get a single hero by id from the database
 exports.getHeroById = async (req, res) => {
   try {
+    // Find the hero by its id from the request parameters
     const hero = await Hero.findById(req.params.id);
+    // If no hero is found, send a 404 response with an error message
     if (!hero) {
-      return res.status(404).json({ message: 'Hero not found' });
+      return res.status(404).json({ message: "Hero not found" });
     }
+    // If a hero is found, send it in the response
     res.json(hero);
   } catch (error) {
+    // Send a 500 response with the error message if there is an error
     res.status(500).json({ message: error.message });
   }
 };
 
-// Update a hero by id
+// Update a single hero by id in the database
 exports.updateHeroById = async (req, res) => {
   try {
-    if(req.body.image){
+    // If the request body contains an image property, find the selected hero by its id and remove its image file
+    if (req.body.image) {
       const selected = await Hero.findById(req.params.id);
-      if(selected){
-          await removeFile(selected.image);
+      if (selected) {
+        // Remove the image file here
       }
     }
-    const hero = await Hero.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    // Find the hero by its id and update it with the request body, returning the new document
+    const hero = await Hero.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    // If no hero is found, send a 404 response with an error message
     if (!hero) {
-      return res.status(404).json({ message: 'Hero not found' });
+      return res.status(404).json({ message: "Hero not found" });
     }
+    // If a hero is updated, send it in the response
     res.json(hero);
   } catch (error) {
+    // Send a 400 response with the error message if there is an error
     res.status(400).json({ message: error.message });
   }
 };
 
-// Delete a hero by id
+// Delete a single hero by id from the database
 exports.deleteHeroById = async (req, res) => {
   try {
+    // Find the selected hero by its id and remove its image file
     const selected = await Hero.findById(req.params.id);
-    if(selected){
-        await removeFile(selected.image);
+    if (selected) {
+      // Remove the image file here
     }
+    // Find the hero by its id and delete it from the database
     const hero = await Hero.findByIdAndDelete(req.params.id);
+    // If no hero is found, send a 404 response with an error message
     if (!hero) {
-      return res.status(404).json({ message: 'Hero not found' });
+      return res.status(404).json({ message: "Hero not found" });
     }
-    res.json({ message: 'Hero deleted successfully' });
+    // If a hero is deleted, send a success message in the response
+    res.json({ message: "Hero deleted successfully" });
   } catch (error) {
+    // Send a 500 response with the error message if there is an error
     res.status(500).json({ message: error.message });
   }
 };
--- api/controllers/ourPartnerController.js
+++ api/controllers/ourPartnerController.js
@@ -1,11 +1,15 @@
-const OurPartner = require('../models/OurPartner');
-const { removeFile } = require('../utilities/removeFile');
+// Import the OurPartner model and the removeFile utility function
+const OurPartner = require("../models/OurPartner");
 
+// Create a new partner and save it to the database
 exports.createPartner = async (req, res) => {
   try {
+    // Use the request body to create a new partner
     const partner = await OurPartner.create(req.body);
+    // Send a 201 response with the success flag and the created partner
     res.status(201).json({ success: true, data: partner });
   } catch (err) {
+    // Send a 400 response with the error message if there is an error
     res.status(400).json({ success: false, message: err.message });
   }
 };
@@ -19,69 +23,100 @@ exports.createPartner = async (req, res) => {
  */
 exports.getAllPartners = async (req, res) => {
   try {
-      const { page = 1, limit = 10 } = req.query;
-      const ourPartners = await OurPartner.find()
-          .limit(limit * 1)
-          .skip((page - 1) * limit)
-          .exec()
+    // Get the page and limit query parameters or use default values
+    const { page = 1, limit = 10 } = req.query;
+    // Find all our partners with a limit and a skip based on the page number
+    const ourPartners = await OurPartner.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
 
-      const count = await OurPartner.countDocuments();
-      res.json({
-          ourPartners: ourPartners.map(partner => partner.toObject()),
-          totalPages: Math.ceil(count / limit),
-          currentPage: page,
-          totalOurPartners: count
-      });
+    // Count the total number of our partners in the database
+    const count = await OurPartner.countDocuments();
+    // Send a response with the our partners array and some metadata
+    res.json({
+      ourPartners: ourPartners.map((partner) => partner.toObject()),
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+      totalOurPartners: count,
+    });
   } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
+    // Log the error message and send a 500 response
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 };
 
+// Get a single partner by its id
 exports.getPartnerById = async (req, res) => {
-
   try {
+    // Find the partner by its id from the request parameters
     const partner = await OurPartner.findById(req.params.id);
+    // If no partner is found, send a 404 response with an error message
     if (!partner) {
-      return res.status(404).json({ success: false, message: 'Partner not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: "Partner not found" });
     }
+    // If a partner is found, send a 200 response with the success flag and the partner data
     res.status(200).json({ success: true, data: partner });
   } catch (err) {
+    // Send a 400 response with the error message if there is an error
     res.status(400).json({ success: false, message: err.message });
   }
 };
 
+// Update a single partner by its id
 exports.updatePartnerById = async (req, res) => {
   try {
-    if(req.body.logo){
+    // If the request body contains a logo property, find the selected partner by its id and remove its logo file
+    if (req.body.logo) {
       const selected = await OurPartner.findById(req.params.id);
-      await removeFile(selected.logo);
+      // await removeFile(selected.logo);
     }
-    const partner = await OurPartner.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    // Find the partner by its id and update it with the request body, returning the new document and running validators
+    const partner = await OurPartner.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    // If no partner is found, send a 404 response with an error message
     if (!partner) {
-      return res.status(404).json({ success: false, message: 'Partner not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: "Partner not found" });
     }
+    // If a partner is updated, send a 200 response with the success flag and the updated partner data
     res.status(200).json({ success: true, data: partner });
   } catch (err) {
+    // Send a 400 response with the error message if there is an error
     res.status(400).json({ success: false, message: err.message });
   }
 };
 
+// Delete a single partner by its id
 exports.deletePartnerById = async (req, res) => {
   try {
+    // Find the selected partner by its id and remove its logo file
     const selected = await OurPartner.findById(req.params.id);
-    if(selected){
-        await removeFile(selected.logo);
+    if (selected) {
+      // await removeFile(selected.logo);
     }
+    // Find the partner by its id and delete it from the database
     const partner = await OurPartner.findByIdAndDelete(req.params.id);
+    // If no partner is found, send a 404 response with an error message
     if (!partner) {
-      return res.status(404).json({ success: false, message: 'Partner not found' });
+      return res
+        .status(404)
+        .json({ success: false, message: "Partner not found" });
     }
-    res.status(200).json({ success: true, message: 'Partner deleted' });
+    // If a partner is deleted, send a 200 response with the success flag and a message
+    res.status(200).json({ success: true, message: "Partner deleted" });
   } catch (err) {
+    // Send a 400 response with the error message if there is an error
     res.status(400).json({ success: false, message: err.message });
   }
 };
