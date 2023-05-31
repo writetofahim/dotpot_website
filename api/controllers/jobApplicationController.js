0371e1344b19f8fcd0e0564fa43176f51c820dfb
Md Tonmoy <tanzinulkabir@gmail.com>
2023-05-31 17:01:32 +0600

    jobApplicationController workController warning fixed

--- api/controllers/jobApplicationController.js
+++ api/controllers/jobApplicationController.js
@@ -1,104 +1,119 @@
-const JobApplication = require('../models/JobApplication');
-const {removeFile}  = require('../utilities/removeFile');
+// Import the JobApplication model
+const JobApplication = require("../models/JobApplication");
 
+// Define a function to create a new job application and save it to the database
 const createJobApplication = async (req, res) => {
   try {
+    // Create a new JobApplication instance from the request body
     const jobApplication = new JobApplication(req.body);
-
+    // Save the job application to the database
     await jobApplication.save();
-
+    // Send a success response with the created job application
     res.status(201).json({ success: true, data: jobApplication });
   } catch (error) {
+    // If there's an error, send a failure response with the error message
     res.status(500).json({ success: false, error: error.message });
   }
 };
 
+// Define a function to get all job applications from the database with pagination
 const getAllJobApplications = async (req, res) => {
-    // Get the current page number from the query parameters, default to 1 if not specified
-    const page = parseInt(req.query.page) || 1;
-    // Get the number of Orders to display per page from the query parameters, default to 10 if not specified
-    const limit = parseInt(req.query.limit) || 10;
-  
-    try {
-      // Get the total number of works in the database
-      const applications = await JobApplication.countDocuments({});
-      // Calculate the total number of pages based on the limit and total number of JobApplications
-      const totalPages = Math.ceil(applications / limit);
-      // Calculate the starting index of the JobApplications to retrieve
-      const startIndex = (page - 1) * limit;
-      // Calculate the ending index of the JobApplications to retrieve
-      const endIndex = page * limit;
-  
-      // Retrieve the JobApplications from the database based on the pagination parameters
-      const JobApplications = await JobApplication.find({}).sort({ createdAt: -1 }).skip(startIndex).limit(limit)
-      // Send the JobApplications as a response along with metadata about the pagination
-      res.json({
-        applications: applications,
-        totalPages,
-        currentPage: page,
-        JobApplications: JobApplications
-      });
-    } catch (err) {
-      // If there's an error, log it to the console and send a 500 response
-      console.log(err);
-      res.status(500).send({ error: 'Server error' });
-    }
+  // Get the current page number from the query parameters, default to 1 if not specified
+  const page = parseInt(req.query.page) || 1;
+  // Get the number of Orders to display per page from the query parameters, default to 10 if not specified
+  const limit = parseInt(req.query.limit) || 10;
+  try {
+    // Get the total number of works in the database
+    const applications = await JobApplication.countDocuments({});
+    // Calculate the total number of pages based on the limit and total number of JobApplications
+    const totalPages = Math.ceil(applications / limit);
+    // Calculate the starting index of the JobApplications to retrieve
+    const startIndex = (page - 1) * limit;
+    // Calculate the ending index of the JobApplications to retrieve
+    const endIndex = page * limit;
+    // Retrieve the JobApplications from the database based on the pagination parameters
+    const JobApplications = await JobApplication.find({})
+      .sort({ createdAt: -1 })
+      .skip(startIndex)
+      .limit(limit);
+    // Send the JobApplications as a response along with metadata about the pagination
+    res.json({
+      applications: applications,
+      totalPages,
+      currentPage: page,
+      JobApplications: JobApplications,
+    });
+  } catch (err) {
+    // If there's an error, log it to the console and send a 500 response
+    console.log(err);
+    res.status(500).send({ error: "Server error" });
+  }
 };
 
+// Define a function to get a single job application by its id from the database
 const getJobApplicationById = async (req, res) => {
   try {
+    // Find the job application by its id in the request parameters
     const jobApplication = await JobApplication.findById(req.params.id);
-
+    // If no job application is found, send a not found response
     if (!jobApplication) {
-      return res.status(404).json({ success: false, error: 'Job application not found' });
+      return res
+        .status(404)
+        .json({ success: false, error: "Job application not found" });
     }
-
+    // Send a success response with the found job application
     res.status(200).json({ success: true, data: jobApplication });
   } catch (error) {
+    // If there's an error, send a failure response with the error message
     res.status(500).json({ success: false, error: error.message });
   }
 };
 
+// Define a function to update a single job application by its id in the database
 const updateJobApplicationById = async (req, res) => {
   try {
+    // Find and update the job application by its id in the request parameters and body, return the updated document and run validators
     const jobApplication = await JobApplication.findByIdAndUpdate(
       req.params.id,
       req.body,
-      {
-        new: true,
-        runValidators: true,
-      },
+      { new: true, runValidators: true }
     );
-
+    // If no job application is found, send a not found response
     if (!jobApplication) {
-      return res.status(404).json({ success: false, error: 'Job application not found' });
+      return res
+        .status(404)
+        .json({ success: false, error: "Job application not found" });
     }
-
+    // Send a success response with the updated job application
     res.status(200).json({ success: true, data: jobApplication });
   } catch (error) {
+    // If there's an error, send a failure response with the error message
     res.status(500).json({ success: false, error: error.message });
   }
 };
 
+// Define a function to delete a single job application by its id from the database
 const deleteJobApplicationById = async (req, res) => {
   try {
-    const selected = await JobApplication.findById(req.params.id);
-    if(selected){
-        await removeFile(selected.resume);
-    }
-    
-    const jobApplication = await JobApplication.findByIdAndDelete(req.params.id);
-
+    // Find and delete the job application by its id in the request parameters
+    const jobApplication = await JobApplication.findByIdAndDelete(
+      req.params.id
+    );
+    // If no job application is found, send a not found response
     if (!jobApplication) {
-      return res.status(404).json({ success: false, error: 'Job application not found' });
+      return res
+        .status(404)
+        .json({ success: false, error: "Job application not found" });
     }
-
+    // Send a success response with an empty data object
     res.status(200).json({ success: true, data: {} });
   } catch (error) {
+    // If there's an error, send a failure response with the error message
     res.status(500).json({ success: false, error: error.message });
   }
 };
 
+// Export the functions as a module
 module.exports = {
   createJobApplication,
   getAllJobApplications,
--- api/controllers/workController.js
+++ api/controllers/workController.js
@@ -1,13 +1,23 @@
+// Import the Work model
 const Work = require("../models/Work");
-const { removeFile } = require("../utilities/removeFile");
 
 // Create a work
+/**
+@params {Object} req - The request object.
+@params {Object} res - The response object.
+@returns {Object} - Returns a JSON object containing a success message and the created work.
+@description Creates a new work from the request body and saves it to the database.
+*/
 exports.createWork = async (req, res) => {
   try {
+    // Create a new Work instance from the request body
     const work = new Work(req.body);
+    // Save the work to the database
     await work.save();
+    // Send a success response with the created work
     res.status(201).json({ message: "Work created successfully", work });
   } catch (error) {
+    // If there's an error, log it to the console and send a failure response
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
@@ -58,54 +68,87 @@ exports.getAllWorks = async (req, res) => {
 };
 
 // Get a specific work by ID
+/**
+@params {Object} req - The request object.
+@params {Object} res - The response object.
+@returns {Object} - Returns a JSON object containing a single work or an error message if not found.
+@description Finds a work by its id in the request parameters and sends it as a response.
+*/
 exports.getWorkById = async (req, res) => {
   try {
+    // Find the work by its id in the request parameters
     const work = await Work.findById(req.params.id);
+    // If no work is found, send a not found response
     if (!work) {
       return res.status(404).json({ message: "Work not found" });
     }
+    // Send a success response with the found work
     res.status(200).json({ work });
   } catch (error) {
+    // If there's an error, log it to the console and send a failure response
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
 // Update a work by ID
+/**
+@params {Object} req - The request object.
+@params {Object} res - The response object.
+@returns {Object} - Returns a JSON object containing a success message and the updated work or an error message if not found.
+@description Updates a work by its id in the request parameters and body and sends it as a response.
+*/
 exports.updateWorkById = async (req, res) => {
   try {
+    // If the request body contains an image, find the work by its id and remove the old image
     if (req.body.image) {
       const selected = await Work.findById(req.params.id);
-      if (selected) {
-        await removeFile(selected.image);
-      }
+      // if (selected) {
+      //   await removeFile(selected.image);
+      // }
     }
+    // Find and update the work by its id in the request parameters and body, return the updated document
     const work = await Work.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
+    // If no work is found, send a not found response
     if (!work) {
       return res.status(404).json({ message: "Work not found" });
     }
+    // Send a success response with the updated work
     res.status(200).json({ message: "Work updated successfully", work });
   } catch (error) {
+    // If there's an error, log it to the console and send a failure response
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
 // Delete a work by ID
+/**
+@params {Object} req - The request object.
+@params {Object} res - The response object.
+@returns {Object} - Returns a JSON object containing a success message or an error message if not found.
+@description Deletes a work by its id in the request parameters and sends a confirmation message as a response.
+*/
 exports.deleteWorkById = async (req, res) => {
   try {
+    // Find the work by its id in the request parameters
     const selected = await Work.findById(req.params.id);
+    // If the work has an image, remove it from the file system
     // if(selected){
     //     await removeFile(selected.image);
     // }
+    // Delete the work from the database
     const work = await Work.findByIdAndDelete(req.params.id);
+    // If no work is found, send a not found response
     if (!work) {
       return res.status(404).json({ message: "Work not found" });
     }
+    // Send a success response with a confirmation message
     res.status(200).json({ message: "Work deleted successfully" });
   } catch (error) {
+    // If there's an error, log it to the console and send a failure response
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
